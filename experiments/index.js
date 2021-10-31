1c99f42ed863a1eb7bb0681d61a9535e93e07dbb
Valentyn <xdshkaa2@gmail.com>
2021-10-31 17:37:25 +0200

    fixes

--- experiments/index.js
+++ experiments/index.js
@@ -1,5 +1,5 @@
-const analyzeAggTrades = require('./analyze-agg-trades');
-const analyzeAggTrade2 = require('./analyze-agg-trades2');
+// const analyzeAggTrades = require('./analyze-agg-trades');
+// const analyzeAggTrade2 = require('./analyze-agg-trades2');
 
 module.exports = () => {
   // analyzeAggTrades();
--- migrations/index.js
+++ migrations/index.js
@@ -1,11 +1,11 @@
-const loadDailyKlines = require('./load-daily-klines');
-const createCandlesUsingDailyFiles = require('./create-candles-using-daily-files');
+// const loadDailyKlines = require('./load-daily-klines');
+// const createCandlesUsingDailyFiles = require('./create-candles-using-daily-files');
 
-const findAndRemoveCandleDuplicates = require('./find-and-remove-candle-duplicates');
+// const findAndRemoveCandleDuplicates = require('./find-and-remove-candle-duplicates');
 
-const addVolumeToCandles = require('./add-volume-to-candles');
+// const addVolumeToCandles = require('./add-volume-to-candles');
 
-const loadAndSaveFuturesKlinesHistory = require('./load-and-save-futures-klines-history');
+// const loadAndSaveFuturesKlinesHistory = require('./load-and-save-futures-klines-history');
 
 module.exports = () => {
   // loadDailyKlines();
