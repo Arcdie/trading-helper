9b27f80432babe32248887907e491b10baf337c9
Valentyn <xdshkaa2@gmail.com>
2021-11-01 23:58:04 +0200

    public-version: fixes

--- controllers/instruments/utils/get-active-instruments.js
+++ controllers/instruments/utils/get-active-instruments.js
@@ -5,14 +5,18 @@ const getActiveInstruments = async ({
 }) => {
   const matchObj = {
     is_active: true,
-    // name: 'IOTXUSDTPERP',
+    name: 'IOTAUSDTPERP',
   };
 
   if (isOnlyFutures) {
     matchObj.is_futures = true;
   }
 
-  const instrumentsDocs = await InstrumentNew.find(matchObj).exec();
+  const instrumentsDocs = await InstrumentNew
+    .find(matchObj)
+    // .sort({ name: 1 })
+    // .limit(10)
+    .exec();
 
   return {
     status: true,
